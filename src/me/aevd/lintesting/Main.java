50e843d56eecfd796fd374a39df9399f5e040520
Anton Evdokimov <free0u@gmail.com>
2015-03-14 21:42:18 +0300

    Testing queue

--- src/me/aevd/lintesting/Main.java
+++ src/me/aevd/lintesting/Main.java
@@ -1,11 +1,13 @@
 package me.aevd.lintesting;
 
-import me.aevd.lintesting.transfer.AccountsSynchronized;
+import me.aevd.lintesting.queue.QueueWithoutAnySync;
 import me.aevd.lintesting.util.Caller;
 
 public class Main {
-    public static void main(String[] args) {
-        Caller caller = new AccountsCaller(AccountsSynchronized.class);
+    public static void main(String[] args) throws NoSuchMethodException {
+//        Caller caller = new QueueCaller(AccountsSynchronized.class);
+
+        Caller caller = new QueueCaller(QueueWithoutAnySync.class);
         Tester tester = new Tester(caller);
         tester.test();
     }
