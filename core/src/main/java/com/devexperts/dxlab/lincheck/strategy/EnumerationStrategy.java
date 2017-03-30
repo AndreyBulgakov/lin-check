d588c0a8897bb213b7a359964b8cebc29ad61f8e
m13oas <aozertsov@gmail.com>
2017-03-30 15:46:53 +0300

    general enumeration strategy

--- core/src/main/java/com/devexperts/dxlab/lincheck/strategy/EnumerationStrategy.java
+++ core/src/main/java/com/devexperts/dxlab/lincheck/strategy/EnumerationStrategy.java
@@ -1,25 +1,28 @@
 package com.devexperts.dxlab.lincheck.strategy;
 
 import com.devexperts.dxlab.lincheck.LinCheckThread;
+import javafx.util.Pair;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static java.nio.file.StandardOpenOption.APPEND;
 
+/**
+ * Strategy
+ */
 public class EnumerationStrategy implements Strategy {
     private Map<CheckPoint, Set<CheckPoint>> passedPaths = new HashMap<>();
     private CheckPoint firstCheckPoint;
     PrintStream logger = getLogger();
-    private volatile int currentThread = 1;
+    private volatile int currentThread;
     private volatile int wasInterleavings = 0;
+    private volatile Pair<Integer, Integer> interleavingThreads;
+    private volatile List<Integer> executionQueue;
     private volatile boolean needInterleave = true;
     private volatile boolean needChangeFirstThread = false;
 
@@ -33,6 +36,7 @@ public class EnumerationStrategy implements Strategy {
             logger.println("\t\tThread id: " + th.getThreadId() + " currentID: " + currentThread);
             logger.println("\t\tCurrentLocation id" + location);
             CheckPoint currentPoint = new CheckPoint(location, th.getThreadId());
+
             onSharedVariableAccess(currentPoint);
             //переключились и снова ждем, когда можно будет продолжить выполнение
             while (th.getThreadId() != currentThread){}
@@ -54,13 +58,108 @@ public class EnumerationStrategy implements Strategy {
         }
     }
 
-    public void printHeader(int iter, int invocation) {
-        logger.println("Iteration №" + iter + " Invocation №" + invocation + " FirstCheckPoint" + firstCheckPoint);
+    @Override
+    public void endOfThread() {
+        if (Thread.currentThread() instanceof LinCheckThread) {
+            LinCheckThread th = (LinCheckThread) Thread.currentThread();
+            logger.println("\tEndOfThread " + th.getThreadId() + "with interleavings:" + wasInterleavings);
+
+            //В конце потока смотрим, если это поток, который не надо было прерывать - выполняем слеующий в расписании
+            //иначе - выполняем некоторую логику.
+            if (wasInterleavings == 0) {
+                if (currentThread == interleavingThreads.getKey()) {
+                    firstCheckPoint = null;
+                    needInterleave = false;
+                    needChangeFirstThread = true;
+                }
+                int index = executionQueue.indexOf(currentThread) + 1;
+
+                if (index < executionQueue.size()) {
+                    currentThread = executionQueue.get(executionQueue.indexOf(currentThread) + 1);
+                }
+            }
+            else if (wasInterleavings == 1) {
+                firstCheckPoint = null;
+                needInterleave = false;
+                if (currentThread == interleavingThreads.getValue()) {
+                    currentThread = interleavingThreads.getKey();
+                }
+                else if (currentThread == interleavingThreads.getKey()) {
+                    int index = executionQueue.indexOf(currentThread) + 1;
+                    if (index < executionQueue.size() - 1)
+                        currentThread = executionQueue.get(executionQueue.indexOf(currentThread) + 2);
+                }
+                else {
+
+                    int index = executionQueue.indexOf(currentThread) + 1;
+
+                    if (index < executionQueue.size())
+                        currentThread = executionQueue.get(executionQueue.indexOf(currentThread) + 1);
+                }
+            }
+            else if (wasInterleavings == 2) {
+
+                int index = executionQueue.indexOf(currentThread) + 1;
+
+                if (index < executionQueue.size())
+                    currentThread = executionQueue.get(executionQueue.indexOf(currentThread) + 1);
+                }
+            else {
+                logger.println("something else");
+            }
+        }
+    }
+    
+    /**
+     * Method contains logic for interleaving thread
+     * @param currentPoint pair locationId, threadID
+     */
+    private void onSharedVariableAccess(CheckPoint currentPoint) {
+        if (currentThread == interleavingThreads.getKey() || currentThread == interleavingThreads.getValue()) {
+            if (needInterleave) {
+                //если нам нужно найти новую точку и находим - переключаемся
+                if (wasInterleavings == 0 && firstCheckPoint == null && !passedPaths.containsKey(currentPoint)) {
+                    passedPaths.put(currentPoint, new HashSet<>());
+                    firstCheckPoint = currentPoint;
+                    wasInterleavings++;
+                    changeCurrentThread();
+                }
+                //если нужно найти новую точку и не находим - пропускаем
+                else if (wasInterleavings == 0 && firstCheckPoint == null && passedPaths.containsKey(currentPoint)) {
+                }
+                //если не нужно искать новую точку и мы на ней - переключаемся
+                else if (wasInterleavings == 0 && firstCheckPoint != null && currentPoint.equals(firstCheckPoint)) {
+                    wasInterleavings++;
+                    changeCurrentThread();
+                }
+                //если не нужно искать новую точку, но мы не на ней - не переключаемся
+                else if (wasInterleavings == 0 && firstCheckPoint != null && !currentPoint.equals(firstCheckPoint)) {
+                } else if (wasInterleavings == 1 && !passedPaths.get(firstCheckPoint).contains(currentPoint)) {
+                    passedPaths.get(firstCheckPoint).add(currentPoint);
+                    wasInterleavings++;
+                    changeCurrentThread();
+                }
+            }
+        }
     }
 
-    public void returnDatas(int firstThread, boolean needChangeFirstThread){
+    /**
+     * Method for printing information about current invocation
+     * @param iteration - current iteration
+     * @param invocation - current invocation
+     */
+    public void printHeader(int iteration, int invocation) {
+        logger.println("Iteration №" + iteration + " Invocation №" + invocation + " FirstCheckPoint" + firstCheckPoint);
+    }
+
+    /**
+     * Clear information about previous running
+     * @param firstThread
+     * @param needChangeFirstThread
+     */
+    public void prepareInvocation(int firstThread, boolean needChangeFirstThread){
         needInterleave = true;
-        currentThread = firstThread;
+        currentThread = executionQueue.get(0);
         this.needChangeFirstThread = needChangeFirstThread;
         wasInterleavings = 0;
     }
@@ -69,33 +168,24 @@ public class EnumerationStrategy implements Strategy {
         return needChangeFirstThread;
     }
 
-    public void startNewIteration() {
+
+    /**
+     * Method print checked point to log file and clear all information about previous iteration
+     */
+    public void prepareIteration() {
         printTraces();
         firstCheckPoint = null;
         passedPaths = new HashMap<>();
-        returnDatas(1, false);
+        prepareInvocation(1, false);
     }
 
-    @Override
-    public void endOfThread() {
-        if (Thread.currentThread() instanceof LinCheckThread) {
-            LinCheckThread th = (LinCheckThread) Thread.currentThread();
-            logger.println("\tEndOfThread " + th.getThreadId() + "with interleavings:" + wasInterleavings);
-            //если было 1 прерывание => просмотрели всё, нужно менять первую точку
-            if (wasInterleavings == 1) {
-                firstCheckPoint = null;
-                needInterleave = false;
-            }
-            //если было 0 прерываний => закончили смотреть первые переключения, нужно исследовать другие
-            if (wasInterleavings == 0) {
-                firstCheckPoint = null;
-                needInterleave = false;
-                needChangeFirstThread = true;
-            }
-            //меняем поток
-            currentThread = currentThread == 1 ? 2 : 1;
-
-        }
+    public void setExecutionParameters(List<Integer> queue, Pair<Integer, Integer> interleavedThreads) {
+        executionQueue = queue;
+        currentThread = executionQueue.get(0);
+        this.interleavingThreads = interleavedThreads;
+        passedPaths = new HashMap<>();
+        logger.println("Current queue of threads execution" + queue);
+        logger.println("Current interleaving threads id's" + interleavedThreads);
     }
 
     public static PrintStream getLogger() {
@@ -114,39 +204,51 @@ public class EnumerationStrategy implements Strategy {
         return null;
     }
 
+    /**
+     * Print all checked points to log file
+     */
     public void printTraces() {
         passedPaths.forEach((a,b) -> logger.println(a + " with set: " + b));
     }
 
-    private void onSharedVariableAccess(CheckPoint currentPoint) {
-        //TODO переключение потока - одтельный метод со своей логикой
-        if (needInterleave) {
-            //если нам нужно найти новую точку и находим - переключаемся
-            if (wasInterleavings == 0 && firstCheckPoint == null && !passedPaths.containsKey(currentPoint)) {
-                passedPaths.put(currentPoint, new HashSet<>());
-                firstCheckPoint = currentPoint;
-                wasInterleavings++;
-                currentThread = 2 - currentThread + 1;
-            }
-            //если нужно найти новую точку и не находим - пропускаем
-            else if (wasInterleavings == 0 && firstCheckPoint == null && passedPaths.containsKey(currentPoint)) {
-            }
-            //если не нужно искать новую точку и мы на ней - переключаемся
-            else if (wasInterleavings == 0 && firstCheckPoint != null && currentPoint.equals(firstCheckPoint)) {
-                wasInterleavings++;
-                currentThread = 2 - currentThread + 1;
-            }
-            //если не нужно искать новую точку, но мы не на ней - не переключаемся
-            else if (wasInterleavings == 0 && firstCheckPoint != null && !currentPoint.equals(firstCheckPoint)) {
-            }
-            else if (wasInterleavings == 1 && !passedPaths.get(firstCheckPoint).contains(currentPoint)) {
-                passedPaths.get(firstCheckPoint).add(currentPoint);
-                wasInterleavings++;
-                currentThread = 2 - currentThread + 1;
-            }
-        }
+    private void changeCurrentThread() {
+        currentThread = currentThread == interleavingThreads.getKey() ? interleavingThreads.getValue() : interleavingThreads.getKey();
     }
 
+    public static ArrayList<Pair<Integer, Integer>> generateInterleavedPairs(int threadNumber) {
+        ArrayList<Pair<Integer, Integer>> pairList = new ArrayList<>();
+        for (int i = 1; i < threadNumber; i++) {
+            for (int j = i+1; j <= threadNumber; j++) {
+                pairList.add(new Pair<>(i, j));
+            }
+        }
+        return pairList;
+    }
+
+    public static List<List<Integer>> threadPermutations(List<Integer> list) {
+
+        if (list.size() == 0) {
+            List<List<Integer>> result = new ArrayList<List<Integer>>();
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+
+        List<List<Integer>> returnMe = new ArrayList<List<Integer>>();
+        Integer firstElement = list.remove(0);
+        List<List<Integer>> recursiveReturn = threadPermutations(list);
+        for (List<Integer> li : recursiveReturn) {
+            for (int index = 0; index <= li.size(); index++) {
+                List<Integer> temp = new ArrayList<Integer>(li);
+                temp.add(index, firstElement);
+                returnMe.add(temp);
+            }
+        }
+        return returnMe;
+    }
+
+    /**
+     * Class describing Point with respect to location and thread id`s
+     */
     private class CheckPoint{
         private final int location;
         private final int threadId;
@@ -181,9 +283,5 @@ public class EnumerationStrategy implements Strategy {
             result = 31 * result + threadId;
             return result;
         }
-    }
-
-    private enum PointType{
-        READ, WRITE
     }
 }
