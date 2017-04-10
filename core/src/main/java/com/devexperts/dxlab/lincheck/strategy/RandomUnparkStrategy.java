bcbefb48c424cb846e07c012dcea4c12aa9e03d3
AndreyBulgakov <andbul0103@gmail.com>
2017-04-10 23:39:44 +0300

    Worked park and unpark in Strategy + simple fibers pool. Rename lineNumber in ElementId.

---
+++ core/src/main/java/com/devexperts/dxlab/lincheck/strategy/RandomUnparkStrategy.java
@@ -0,0 +1,58 @@
+package com.devexperts.dxlab.lincheck.strategy;
+
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.fibers.Suspendable;
+
+
+public class RandomUnparkStrategy implements Strategy {
+    public static int out;
+
+    @Suspendable
+    @Override
+    public void onSharedVariableRead(int location) {
+        try {
+            if (Fiber.isCurrentFiber()) {
+                Fiber randomFiber = getRandomParkedFiber();
+                Fiber.parkAndUnpark(randomFiber);
+                out++;
+            }
+        } catch (SuspendExecution suspendExecution) {
+            throw new AssertionError(suspendExecution);
+        }
+//        Thread.yield();
+    }
+
+    @Suspendable
+    @Override
+    public void onSharedVariableWrite(int location) {
+        try {
+            if (Fiber.isCurrentFiber()) {
+                Fiber randomFiber = getRandomParkedFiber();
+                Fiber.parkAndUnpark(randomFiber);
+                out++;
+            }
+
+        } catch (SuspendExecution suspendExecution) {
+            throw new AssertionError(suspendExecution);
+        }
+//        Thread.yield();
+    }
+
+    @Suspendable
+    @Override
+    public void endOfThread() {
+        if (Fiber.isCurrentFiber())
+            Fiber.unpark(getRandomParkedFiber());
+    }
+
+    public Fiber getRandomParkedFiber() {
+        Fiber current = Fiber.currentFiber();
+        for (Fiber fiber : StrategyHolder.fibers) {
+            if (fiber != current && !fiber.isTerminated()) {
+                return fiber;
+            }
+        }
+        return current;
+    }
+}
--- core/src/main/java/com/devexperts/dxlab/lincheck/strategy/ThreadYieldStrategy.java
+++ core/src/main/java/com/devexperts/dxlab/lincheck/strategy/ThreadYieldStrategy.java
@@ -1,44 +1,20 @@
 package com.devexperts.dxlab.lincheck.strategy;
 
-import co.paralleluniverse.fibers.Fiber;
-import co.paralleluniverse.fibers.SuspendExecution;
-import co.paralleluniverse.fibers.Suspendable;
+/**
+ * Created by andrey on 4/10/17.
+ */
 
 /**
  * This strategy invokes {@link Thread#yield()} method on every shared variable access.
  */
 public class ThreadYieldStrategy implements Strategy {
-    static int out;
-    @Suspendable
     @Override
     public void onSharedVariableRead(int location) {
-        try {
-
-            if (Fiber.isCurrentFiber()) {
-                Fiber.parkNanos(100);
-                out++;
-//                System.out.println("after park...");
-            }
-
-        } catch (SuspendExecution suspendExecution) {
-            throw new AssertionError(suspendExecution);
-        }
-//        Thread.yield();
+        Thread.yield();
     }
 
-    @Suspendable
     @Override
     public void onSharedVariableWrite(int location) {
-        try {
-            if (Fiber.isCurrentFiber()) {
-                Fiber.parkNanos(100);
-//                Fiber.yield();
-                out++;
-//                System.out.println("after park...");
-            }
-        } catch (SuspendExecution suspendExecution) {
-            throw new AssertionError(suspendExecution);
-        }
-//        Thread.yield();
+        Thread.yield();
     }
 }
